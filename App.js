22b7236441f32e22be8a14f801cddd7df4df3a92
Mikhail Stepanov <Stepanovmm@gmail.com>
2018-05-30 20:31:03 +0300

    made twitter feed with infinite loading

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, ScrollView } from "react-native";
+import { Text, ScrollView, View, ListView, StyleSheet } from "react-native";
 // import { Text, Screen, NavigationBar, ListView, View, ImageBackground, Divider, Tile, Title, Subtitle } from '@shoutem/ui';
 import request from "superagent";
 import { consumerKey, consumerSecretKey } from "./tokens";
@@ -9,14 +9,13 @@ export default class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            data: "not fetched",
-            tweets: [{
-                text: "получала зарплату",
-                user: { name: "Пупа" }
-            }, {
-                text: "за меня",
-                user: { name: "Лупа" }
-            }]
+            data: [],
+            tweets: new ListView.DataSource({
+                rowHasChanged: (row1, row2) => row1 !== row2,
+            }),
+            lastId: undefined,
+            bearerToken: null
+            
         };
     }
 
@@ -33,38 +32,67 @@ export default class App extends Component {
             .set("accept-encoding", "")
             .end((err, res) => {
                 const accessToken = JSON.parse(res.text).access_token;
-                this.setState({data: accessToken}, () => this.getTweets(accessToken));
+                this.setState({bearerToken: accessToken}, () => this.getTweets());
             });
     }
 
-    getTweets(bearerToken) {
+    getTweets = () => {
+        const { bearerToken, lastId } = this.state;
+
+        const queryParams = {
+            count: 10,
+            screen_name: 'twitterapi',
+            max_id: lastId
+        };
+
         request
-            .get('https://api.twitter.com/1.1/statuses/user_timeline.json?count=10&screen_name=twitterapi')
+            .get('https://api.twitter.com/1.1/statuses/user_timeline.json')
+            .query(queryParams)
             .set("Authorization", `Bearer ${bearerToken}`)
             .set("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8")
             .set("accept-encoding", "")
             .end((err, res) => {
-                this.setState({tweets: JSON.parse(res.xhr.response)});
+                const json = JSON.parse(res.xhr.response);
+                this.setState((prevState) => {
+                    const data = [...prevState.data, ...json];
+                    return {
+                        data,
+                        tweets: prevState.tweets.cloneWithRows(data),
+                        lastId: json[json.length - 1].id_str
+                    };
+                });
             });
     }
-
+    
+    renderRow(tweet) { 
+        return (
+            <React.Fragment>
+                <Text style={{fontWeight: "bold"}}>{tweet.user && tweet.user.name}</Text>
+                <Text>{tweet.created_at}</Text>
+                <Text>{tweet.text}</Text>
+            </React.Fragment>
+        );
+    }
 
     render() {
         const { tweets } = this.state;
 
         return(
-            <ScrollView>
-                <Text>{"\n\n"}</Text>
-                {tweets && tweets.map((x, i) => (
-                    <React.Fragment key={i}>
-                        <Text style={{fontWeight: "bold"}}>{x.user && x.user.name}</Text>
-                        <Text>{x.text}</Text>
-                    </React.Fragment>
-                ))}
-                <Text>{"---------------------------------------"}</Text>
-                <Text>{"\n\n"}</Text>
-                <Text>{this.state.data}</Text>
-            </ScrollView>
+            <View style={styles.container}>
+                <ListView
+                    dataSource={tweets}
+                    renderRow={this.renderRow}
+                    onEndReached={this.getTweets}
+                />
+            </View>
         );
     }
 }
+
+var styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        backgroundColor: '#F5FCFF',
+    },  
+});
